4ac04a06101a93adb427b7c6478eaa1a819d358a
swallsy <smwall84@gmail.com>
2017-08-17 21:19:08 -0400

    fixes hover state

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "../styles/Work.css";
 import soundcloudImg from "../styles/img/soundcloud_player.png";
-import mysteryWordsImg from "../styles/img/mysterywords.png";
+import mysteryWordsImg from "../styles/img/mysteryword.png";
 import gabbleImg from "../styles/img/gabble.png";
 import magicShoppeImg from "../styles/img/Magic Shoppe.png";
 export default class Work extends Component {
@@ -52,7 +52,7 @@ export default class Work extends Component {
               <img className="d-block img-fluid col-md-8 project-img" src={mysteryWordsImg} alt="First slide" />
               <div className="col-md-4" id="project-info">
                   <h5>Mystery Words</h5>
-                  <p>Players guess letters until they are able to guess the correct word. If they miss 8 letters, they lose. The app utilizes Apple's dictionary and gives players and option of Easy, Medium or Hard words.</p>
+                  <p>Players guess letters until they are able to guess the correct word. If they miss 8 letters, they lose. The app utilizes Apple's dictionary and gives players an option of Easy, Medium or Hard words.</p>
                   <p>Technologies: Node.js, Express, Mustache</p>
                     <a href="https://pure-citadel-81453.herokuapp.com/start" className="live-btn" target="_blank">Live Site</a>
                     <a href="https://github.com/swallsy/mystery_word_game" className="github-btn" target="_blank" >GitHub</a>
