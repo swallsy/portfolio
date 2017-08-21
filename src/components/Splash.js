c414e84d6b4ec1325446ee3569e3fcffff6b1f3c
swallsy <smwall84@gmail.com>
2017-08-21 18:33:07 -0400

    removes intro splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -12,86 +12,8 @@ import NavBar from "./NavBar.js";
 import "../styles/splash.css";
 
 export default class Splash extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      splash: 1
-    };
-
-    this.increment1 = this.increment1.bind(this);
-    this.increment2 = this.increment2.bind(this);
-    this.increment3 = this.increment3.bind(this);
-  }
-
-  componentDidMount() {
-    var timer1 = setTimeout(this.increment1, 2000);
-    var timer2 = setTimeout(this.increment2, 4000);
-    var timer3 = setTimeout(this.increment3, 6000);
-  }
-
-  increment1 () {
-    this.setState({
-      splash: 2
-    })
-  }
-
-  increment2 () {
-    this.setState({
-      splash: 3
-    })
-  }
-
-  increment3 () {
-    this.setState({
-      splash: 4
-    })
-  }
-
 
   render () {
-    if (this.state.splash === 1) {
-      return (
-        <div className="splash">
-          <div className="container">
-            <section className="col-12-md splash-content">
-              <div className="row">
-                <h1 className="splash-words animated fadeOut fadeIn">
-                Hi, I'm Sara
-                </h1>
-              </div>
-            </section>
-          </div>
-       </div>
-     )
-   } else if (this.state.splash === 2) {
-       return (
-         <div className="splash">
-           <div className="container">
-             <div className="row">
-               <section className="col-12-md splash-content">
-                 <h1 className="splash-words animated fadeOut fadeIn">
-                   I design and build websites
-                 </h1>
-              </section>
-           </div>
-         </div>
-       </div>
-      )
-    } else if (this.state.splash === 3) {
-        return (
-          <div className="splash">
-            <div className="container">
-              <div className="row">
-                <section className="col-12-md splash-content" id="frame3">
-                  <h1 className="splash-words animated fadeOut fadeIn">
-                    using Javascript and <i className="fa fa-heart" id="heart" aria-hidden="true"></i>
-                  </h1>
-               </section>
-            </div>
-          </div>
-        </div>
-       )
-     } else {
         return (
           <div id="main-menu">
             <NavBar />
@@ -105,5 +27,4 @@ export default class Splash extends Component {
           </div>
         )
      }
-  }
 }
