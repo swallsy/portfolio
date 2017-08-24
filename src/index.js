5b0e8b545db43cbc70569bf2288889411f3256e1
swallsy <smwall84@gmail.com>
2017-08-24 17:09:32 -0400

    fixes bug on start

--- src/index.js
+++ src/index.js
@@ -5,25 +5,9 @@ import './styles/App.css';
 
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter, Route, Switch}  from "react-router-dom";
-
-import Splash from "./components/Splash.js";
-import About from "./components/About.js";
-import Work from "./components/Work.js";
-import Contact from "./components/Contact.js";
 import Main from "./components/Main.js";
 
 
-ReactDOM.render (
-  <BrowserRouter>
-      <Switch>
-  			 	<Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/work" component={Work} />
-  				<Route path="/" component={Main} />
-      </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 registerServiceWorker();
