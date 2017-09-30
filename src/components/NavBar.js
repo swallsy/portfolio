a52ec3b8cc160f2aee20d93c236cc746b930efcb
swallsy <smwall84@gmail.com>
2017-09-30 09:29:05 -0400

    fixes the styling in the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,56 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import "../styles/Navbar.css";
 
-
 export default class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animateHamburger: "collapse"
+    };
+  }
+
   render() {
+
     return (
-      <div className="container-fluid">
-        <div className="navbar row" id="navigation-area">
-          <div className="col-md-8 navigation-items">
-            <a href="#about" className="nav-link">
-              ABOUT
-            </a>
-            <a href="#work" className="nav-link">
-              WORK
-            </a>
-            <a href="#contact" className="nav-link" id="last-item">
-              CONTACT
-            </a>
+    <nav className="navbar navbar-inverse navbar-static-top navigation-area" role="navigation">
+        <div className="container">
+          <div className="collapse navbar-collapse hamburger hamburger-navbar" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                  <li><a href="#about" onClick={this.handleNav}>About</a></li>
+                  <li><a href="#work" onClick={this.handleNav}>Work</a></li>
+                  <li><a href="#contact" onClick={this.handleNav}>Contact</a></li>
+              </ul>
           </div>
+            <div className="navbar-header">
+                <button type="button" ref="navToggle" className="navbar-toggler collapsed is-active"
+                 data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                id="hamburger-button">
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+            </div>
         </div>
-      </div>
+    </nav>
+      // <div className="container-fluid">
+      //   <div className="navbar row" id="navigation-area">
+      //     <div className="col-md-8 navigation-items">
+      //       <a href="#about" className="nav-link">
+      //         ABOUT
+      //       </a>
+      //       <a href="#work" className="nav-link">
+      //         WORK
+      //       </a>
+      //       <a href="#contact" className="nav-link" id="last-item">
+      //         CONTACT
+      //       </a>
+      //     </div>
+      //   </div>
+      // </div>
     );
+  }
+
+  handleNav = () => {
+    this.refs.navToggle.click();
   }
 }
