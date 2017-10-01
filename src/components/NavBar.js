5b742fd282b4169deb8008501a3c7ffe946ece49
swallsy <smwall84@gmail.com>
2017-10-01 16:55:08 -0400

    adds animation to hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,41 +13,27 @@ export default class NavBar extends Component {
   render() {
 
     return (
-    <nav className="navbar navbar-inverse navbar-static-top navigation-area" role="navigation">
+    <nav className="navbar navbar-inverse navbar-static-top navigation-area" id="master" role="navigation">
         <div className="container">
-          <div className="collapse navbar-collapse hamburger hamburger-navbar" id="bs-example-navbar-collapse-1">
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div className="hamburger-navbar">
               <ul className="nav navbar-nav">
+                  <li><a href="#a" onClick={this.handleNav}>Home</a></li>
                   <li><a href="#about" onClick={this.handleNav}>About</a></li>
                   <li><a href="#work" onClick={this.handleNav}>Work</a></li>
                   <li><a href="#contact" onClick={this.handleNav}>Contact</a></li>
               </ul>
-          </div>
-            <div className="navbar-header">
-                <button type="button" ref="navToggle" className="navbar-toggler collapsed is-active"
-                 data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                id="hamburger-button">
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
             </div>
-        </div>
+          </div>
+            <button type="button" ref="navToggle" className="navbar-toggler collapsed is-active"
+             data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+            id="hamburger-button">
+              <span className="icon-bar top-bar"></span>
+              <span className="icon-bar middle-bar"></span>
+              <span className="icon-bar bottom-bar"></span>
+            </button>
+          </div>
     </nav>
-      // <div className="container-fluid">
-      //   <div className="navbar row" id="navigation-area">
-      //     <div className="col-md-8 navigation-items">
-      //       <a href="#about" className="nav-link">
-      //         ABOUT
-      //       </a>
-      //       <a href="#work" className="nav-link">
-      //         WORK
-      //       </a>
-      //       <a href="#contact" className="nav-link" id="last-item">
-      //         CONTACT
-      //       </a>
-      //     </div>
-      //   </div>
-      // </div>
     );
   }
 
